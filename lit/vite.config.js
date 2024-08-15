94dbce4644ade472f6c472be5dcb7fac4fb4be82
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-08-15 08:10:31 +0200

    fix: reloading for each file change in public/
    source: https://github.com/vitejs/vite/issues/4970

--- lit/vite.config.js
+++ lit/vite.config.js
@@ -1,4 +1,6 @@
 import { defineConfig } from 'vite';
+import fg from 'fast-glob';
+
 
 export default defineConfig({
   root: '', // Define the new root folder
@@ -10,5 +12,16 @@ export default defineConfig({
         // You can add other HTML files here if needed
       }
     }
-  }
+  },
+  plugins: [
+    {
+      name: 'watch-external', // https://stackoverflow.com/questions/63373804/rollup-watch-include-directory/63548394#63548394
+      async buildStart(){
+        const files = await fg(['src/**/*','public/**/*']);
+        for(let file of files){
+          this.addWatchFile(file);
+        }
+      }
+    }
+  ]
 });
