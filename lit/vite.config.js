d57c66d20056cd171df52b9416c705d2c5b45537
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-08-16 22:53:11 +0200

    feat: generate manifest from template-manifest

--- lit/vite.config.js
+++ lit/vite.config.js
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vite';
-
-function generateManifest() {
-  const manifest = readJsonFile("src/manifest.json");
-  const pkg = readJsonFile("package.json");
-  return {
-    name: pkg.name,
-    description: pkg.description,
-    version: pkg.version,
-    ...manifest,
-  };
-}
+import path from "path";
+import fs from "fs";
 
 export default defineConfig({
   root: '', // Define the new root folder
@@ -23,6 +14,7 @@ export default defineConfig({
     }
   },
   plugins: [
+    generateManifestPlugin('chrome'),
     {
       name: 'watch-external',
       async buildStart(){
@@ -32,3 +24,50 @@ export default defineConfig({
     }
   ]
 });
+
+function generateManifestPlugin(targetBrowser) {
+  return {
+    name: 'generate-manifest',
+    async buildStart(){
+      const srcManifestPath = path.join(__dirname, 'src', 'manifest.json');
+      const distManifestPath = path.join(__dirname, 'dist', 'manifest.json');
+      const pkgPath = path.join(__dirname, 'package.json');
+
+      const srcManifest = readJsonFile(srcManifestPath);
+      const pkg = readJsonFile(pkgPath);
+
+      const manifest = {
+        name: pkg.name,
+        description: pkg.description,
+        version: pkg.version,
+        icons: srcManifest.icons,
+        background: {},
+        ...(targetBrowser === 'chrome' ? {
+          manifest_version: 3,
+          action: srcManifest['{{chrome}}.action'],
+          background: {
+            service_worker: srcManifest['{{chrome}}.service_worker']
+          }
+        } : {
+          manifest_version: 2,
+          browser_action: srcManifest['{{firefox}}.browser_action'],
+          background: {
+            scripts: srcManifest['{{firefox}}.scripts']
+          }
+        })
+      };
+
+      writeJsonFile(distManifestPath, manifest);
+    }
+  }
+}
+
+function readJsonFile(filePath) {
+  const json = fs.readFileSync(filePath, 'utf8');
+  return JSON.parse(json);
+}
+
+function writeJsonFile(filePath, data) {
+  const json = JSON.stringify(data, null, 2);
+  fs.writeFileSync(filePath, json, 'utf8');
+}
