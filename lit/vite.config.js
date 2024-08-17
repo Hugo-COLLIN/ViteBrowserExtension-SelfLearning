dd54eeab263c7a8b81ff4bff3c5e8d8f2d3f906a
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-08-17 02:47:42 +0200

    feat: after build, move html files based on rollupConfig.output (removing src/ in destination path)

--- lit/vite.config.js
+++ lit/vite.config.js
@@ -6,11 +6,13 @@ export default defineConfig({
   root: '', // Define the new root folder
   build: {
     outDir: './dist', // Exit path (relative to new root)
+    emptyOutDir: true, // Empty the output directory before building
+    //define input path (relative to new root)
     rollupOptions: {
       input: {
         index: './src/pages/index.html',
         background: './src/background.ts',
-        // You can add other HTML files here if needed
+        // You can add other files here if needed
       },
       output: {
         entryFileNames: (chunkInfo) => {
@@ -18,13 +20,22 @@ export default defineConfig({
           const folder = path.substring(0, path.lastIndexOf('/'));
           return folder ? `${folder}/[name].js` : '[name].js';
         },
-        chunkFileNames: 'chunks/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
+
+
+        // entryFileNames: (chunkInfo) => {
+        //   const path = chunkInfo.facadeModuleId.split('/src/').pop();
+        //   const folder = path.substring(0, path.lastIndexOf('/'));
+        //
+        //   return folder ? `${folder}/[name].js` : '[name].js';
+        // },
+        // chunkFileNames: 'chunks/[name]-[hash].js',
+        // assetFileNames: 'assets/[name]-[hash].[ext]',
+      },
     }
   },
   plugins: [
     generateManifestPlugin('chrome'),
+    rollupOutputBasedHtmlFilesLocationPlugin(),
     {
       name: 'watch-external',
       async buildStart(){
@@ -34,6 +45,25 @@ export default defineConfig({
     }
   ]
 });
+
+function rollupOutputBasedHtmlFilesLocationPlugin() {
+  return {
+    name: 'move-html-files-based-on-rollup-output',
+    writeBundle(options, bundle) {
+      Object.keys(bundle).forEach(fileName => {
+        if (fileName.endsWith('.html')) {
+          const srcPath = path.resolve(options.dir, fileName);
+          // destPath is srcPath but without the 'src/' part
+          const destPath = path.resolve(options.dir, fileName.replace('src/', ''));
+          // const destPath = path.resolve(options.dir, path.dirname(fileName), 'pages', path.basename(fileName));
+          fs.mkdirSync(path.dirname(destPath), {recursive: true});
+          fs.renameSync(srcPath, destPath);
+        }
+      });
+      fs.rmdirSync(path.resolve(options.dir, 'src'), {recursive: true});
+    }
+  };
+}
 
 function generateManifestPlugin(targetBrowser) {
   return {
