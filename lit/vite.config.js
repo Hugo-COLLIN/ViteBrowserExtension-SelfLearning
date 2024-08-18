b81bdb59be2e45077bcb473af1a8b50961610b93
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-08-18 23:18:44 +0200

    refactor: generate license list

--- lit/vite.config.js
+++ lit/vite.config.js
@@ -3,41 +3,6 @@ import path from "path";
 import fs from "fs";
 import { exec } from 'child_process';
 
-function generateLicensesPlugin() {
-  return {
-    name: 'generate-licenses',
-    async buildStart() {
-      exec('npx license-checker --production --json', (error, stdout, stderr) => {
-        if (error) {
-          console.error(`exec error: ${error}`);
-          return;
-        }
-
-        const licenses = JSON.parse(stdout);
-
-        let formatted = '';
-        for (let dep in licenses) {
-          formatted += `${dep}\n`;
-
-          // Filter out 'path' and 'licenseFile' before the loop
-          let props = Object.keys(licenses[dep]).filter(prop => prop !== 'path' && prop !== 'licenseFile');
-
-          for (let i = 0; i < props.length; i++) {
-            let prop = props[i];
-            let prefix = (i === props.length - 1) ? '└─' : '├─';
-            formatted += `${prefix} ${prop}: ${licenses[dep][prop]}\n`;
-          }
-          formatted += '\n';
-        }
-
-
-        fs.writeFileSync(path.join('dist/', 'licenses.txt'), formatted);
-      });
-    }
-  };
-
-}
-
 export default defineConfig(({mode}) => {
   const targetBrowser = process.env.TARGET || 'chrome';
 
@@ -199,4 +164,39 @@ function readJsonFile(filePath) {
 function writeJsonFile(filePath, data) {
   const json = JSON.stringify(data, null, 2);
   fs.writeFileSync(filePath, json, 'utf8');
+}
+
+function generateLicensesPlugin() {
+  return {
+    name: 'generate-licenses',
+    async buildStart() {
+      exec('npx license-checker --production --json', (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+
+        const licenses = JSON.parse(stdout);
+
+        let formatted = '';
+        for (let dep in licenses) {
+          formatted += `${dep}\n`;
+
+          // Filter out 'path' and 'licenseFile' before the loop
+          let props = Object.keys(licenses[dep]).filter(prop => prop !== 'path' && prop !== 'licenseFile');
+
+          for (let i = 0; i < props.length; i++) {
+            let prop = props[i];
+            let prefix = (i === props.length - 1) ? '└─' : '├─';
+            formatted += `${prefix} ${prop}: ${licenses[dep][prop]}\n`;
+          }
+          formatted += '\n';
+        }
+
+
+        fs.writeFileSync(path.join('dist/', 'licenses.txt'), formatted);
+      });
+    }
+  };
+
 }
