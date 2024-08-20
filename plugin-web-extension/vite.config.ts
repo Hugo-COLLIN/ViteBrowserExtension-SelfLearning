d04f86d3f90696157c90c19522ae3cec819db8b6
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-08-20 21:57:13 +0200

    chore:rename page to options

--- plugin-web-extension/vite.config.ts
+++ plugin-web-extension/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
     webExtension({
       manifest: generateManifest,
       watchFilePaths: ["package.json", "manifest.json"],
+      disableAutoLaunch: true,
     }),
   ],
 });
